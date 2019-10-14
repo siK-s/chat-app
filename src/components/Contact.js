05dcd72b8039a74f8c641b6a70bdaf0056a7fb21
Atayi <atayisika@gmail.com>
2019-10-14 16:06:53 +0200

    Modification du composant Contact de sorte à pouvoir modifier le status du contact en cliquant sur l'icon ou le texte du status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Contact.css';
 
-function Contact(props) {
-    return (
-        <div className='Contact'>
-            <img className='avatar' src={props.image} alt={props.name} />
-            <div>
-                <h4 className='name'>{props.name}</h4>
-                <div className='status'>
-                    <span className={props.online ? 'status-online' : 'status-offline'}></span>
-                    <span className='status-text'>{props.online ? 'Online' : 'Offline'}</span>
+class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {online : props.online};
+    }
+    render() {
+        const changeStatus = (e) => {
+            const status = !this.state.online;
+            this.setState({online : status});
+        }
+        return (
+            <div className='Contact'>
+                <img className='avatar' src={this.props.image} alt={this.props.name} />
+                <div>
+                    <h4 className='name'>{this.props.name}</h4>
+                    <div className='status' onClick={changeStatus}>
+                        <span className={this.state.online ? 'status-online' : 'status-offline'}></span>
+                        <span className='status-text'>{this.state.online ? 'Online' : 'Offline'}</span>
+                    </div>
                 </div>
             </div>
-            
-        </div>
-    );
+        );
+    }
 }
 
 export default Contact;
